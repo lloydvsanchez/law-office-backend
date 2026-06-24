0f2286c7186d69e89f2be3275e461fc34df6304e
Lloyd Sanchez <lloydvsanchez@gmail.com>
2026-06-24 13:58:37 +0800

    update for render.com

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins "http://localhost:5173",
+            "https://law-office-frontend.lloydvsanchez.workers.dev"
 
     resource "*",
       headers: :any,
